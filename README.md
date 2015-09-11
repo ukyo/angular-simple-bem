58c4723a8a8f18b48421e5f7b73cde079bcdcfd5
ukyo <ukyo.web@gmail.com>
2015-09-11 16:36:31 +0900

    v0.1.0

--- README.md
+++ README.md
@@ -1,1 +1,65 @@
 # angular-simple-bem
+
+angular-simple-bem is a BEM library that is easy to write and easy to read.
+
+# Install
+
+```
+bower install --save angular-simple-bem
+```
+
+# Usage
+
+Example: http://plnkr.co/edit/upFcR8fMcbPaHsERhO7l?p=preview
+
+## Nest block or element
+
+```html
+<div bem="block">
+  <div bem="__elem1">
+    <div bem="__elem2">nest block or element</div>
+  </div>
+</div>
+```
+
+```html
+<div bem="block" class="block">
+  <div bem="__elem1" class="block__elem1">
+    <div bem="__elem2" class="block__elem1__elem2">nest block or element</div>
+  </div>
+</div>
+```
+
+## Modifier
+
+```html
+<div bem="block--mod">
+  <div bem="__elem--mod1,mod2">modifier</div>
+</div>
+```
+
+```html
+<div bem="block--mod" class="block block--mod">
+  <div bem="__elem--mod1,mod2" class="block__elem block__elem--mod1 block__elem--mod2">modifier</div>
+</div>
+```
+
+## Modifier with binding
+
+```html
+<div ng-init="foo = true; bar = false;">
+  <div bem="block--mod1:foo,mod2:bar">modifier with binding</div>
+  <div bem="block--::mod1:foo,mod2:bar">modifier with one time binding</div>
+</div>
+```
+
+```html
+<div ng-init="foo = true; bar = false;">
+  <div bem="block--mod1:foo,mod2:bar" class="block block--mod1">modifier with binding</div>
+  <div bem="block--::mod1:foo,mod2:bar" class="block block--mod1">modifier with one time binding</div>
+</div>
+```
+
+# License
+
+MIT License
---
+++ angular-simple-bem.js
@@ -0,0 +1,63 @@
+/*!
+ * angular-simple-bem.js 0.1.0 - MIT License. https://github.com/ukyo/angular-simple-bem/blob/master/LICENSE
+ */
+
+angular.module('angular-simple-bem', [])
+
+.directive('bem', function() {
+  'use strict';
+
+  var BASE_DEFINITION = '$angular-simple-bem-base-definition';
+
+  function getParentDefinition(el) {
+    return el.parent().data(BASE_DEFINITION) || getParentDefinition(parent);
+  }
+
+  function filterRawModifier(m) {
+    return m.indexOf(':') === -1;
+  }
+
+  function filterBoolModifier(m) {
+    return m.indexOf(':') !== -1;
+  }
+
+  function concatString(s1, s2) {
+    return s1 + s2;
+  }
+
+  function stringifyKey(s) {
+    var match = s.match(/^([^:]+):([\s\S]+)$/);
+    return '"' + match[1].trim() + '":' + match[2];
+  }
+
+  return {
+    restrict: 'A',
+    compile: function(tElement, tAttr) {
+      var match, blockOrElement, be, m, modifiers, rawModifiers, boolModifiers, cs, oneTimeBinding;
+
+      match = tAttr.bem.trim().match(/^([\s\S]*?)(?:--(::)?([\s\S]*))?$/);
+      if (!match) throw new Error('bem: invalid pattern');
+      be = match[1].trim();
+      m = (match[3] || '').trim();
+      oneTimeBinding = match[2] || '';
+
+      blockOrElement = /^__/.test(be) ? getParentDefinition(tElement) + be : be;
+      tElement.data(BASE_DEFINITION, blockOrElement);
+
+      modifiers = m ? m.split(/\s*,\s*/) : [];
+      rawModifiers = modifiers.filter(filterRawModifier);
+      boolModifiers = modifiers.filter(filterBoolModifier);
+
+      cs = concatString.bind(null, blockOrElement + '--');
+      tElement.addClass([blockOrElement].concat(rawModifiers.map(cs)).join(' '));
+
+      return function bemLink(scope, element) {
+        scope.$watch(oneTimeBinding + '{' + boolModifiers.map(cs).map(stringifyKey).join() + '}', function bemWatchAction(newValue) {
+          angular.forEach(newValue, function(v, k) {
+            element.toggleClass(k, v);
+          });
+        }, true);
+      };
+    }
+  };
+});
